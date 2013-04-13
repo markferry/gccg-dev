495803ab63d00bf528ec1799228df49c272b1124
vastorperedhil <vastorperedhil@634a58c5-d05b-43e7-87bc-c68b3b3f1831>
2013-04-13 14:52:25 +0000

    git-svn-id: https://gccg.svn.sourceforge.net/svnroot/gccg/core@691 634a58c5-d05b-43e7-87bc-c68b3b3f1831

--- MetwRW.bat
+++ MetwRW.bat
@@ -4,4 +4,4 @@ set USER=Windows
 set CLIENT=.\ccg_client.exe
 if exist .\home set HOME=.\home
 if exist module_windows32\ccg_client.exe set CLIENT=module_windows32\ccg_client.exe
-start %CLIENT% --server gccg.councilofelrond.org --design 1920x1050 --user %USER% %1 %2 %3 %4 %5 %6 %7 %8 %9  metwDC.xml
+start %CLIENT% --design 1920x1050 --user %USER% %1 %2 %3 %4 %5 %6 %7 %8 %9  metwRW.xml
